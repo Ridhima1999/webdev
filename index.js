f0bee99a143f9b5247caab538044ef76ca23e0c9
Ridhima1999 <ridhimamehta99@gmail.com>
2020-07-28 20:28:30 +0530

    changes to resume.html

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app=express();
+app.use(express.static(__dirname+'/frontend'))
+
 app.get('/',function(req,res){
-    res.send("hello world");
+    res.sendFile(__dirname+'/frontend/html/index.html');
+})
+app.get('/clock',function(req,res){
+    res.sendFile(__+dirname+'/frontend/html/clock.html');
 })
 
-var port=process.env.PORT || 3000;
+var port=process.env.PORT || 5000;
 app.listen(port,function(){
     console.log('site run on http://localhost:'+port);
 });
